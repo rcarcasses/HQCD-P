f64848656e181d3542efbd044470796fab6e9f03
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2018-02-27 22:22:08 +0000

    Normalizable modes are now normalized properly. Test it.

--- R/U1Modes.R
+++ R/U1Modes.R
@@ -4,9 +4,15 @@
     list(c(dy1, dy2))
   }
 
+  odeFunAdS <- function(x, y, pars) {
+      dy1 <- y[2]
+      dy2 <- - mass * mass * y[1] + y[2] / x
+      list(c(dy1, dy2))
+  }
+
   odeFun1NormalizableModes <- function(x, y, pars) {
-    dy2 <- -mass * mass * y[1] - (Asder1fun(x) - Phider1fun(x)) * y[2]
     dy1 <- y[2]
+    dy2 <- - mass * mass * y[1] - (Asder1fun(x) - Phider1fun(x)) * y[2]
     list(c(dy1, dy2))
   }
 
@@ -34,7 +40,8 @@ precompute <- function(alpha) {
   fact0fun     <- splinefun(z, (1 - 2 * exp(-2 * As) * alpha * Asder2) / (1 - 2 * exp(-2 * As) * alpha * Asder1^2))
   fact1fun     <- splinefun(z, exp(2 * As) - 2 * alpha * Asder2)
   originalPars <- list(A0 = A0, h = h, zmax = zmax)
-  list(ihqcd = ihqcd, originalPars = originalPars , env = environment())
+  list(ihqcd = ihqcd, originalPars = originalPars , env = environment(),
+       Asder1fun = Asder1fun, Phider1fun = Phider1fun)
 }
 
 # these are the non-normalizable modes
@@ -72,20 +79,16 @@ getFQs <- function(Q2, m) {
 
 getFQNormalizableModes <- function(mass, m) {
   # also compute some useful combinations
-  fQ      <- splinefun(m$x, m$y)
-  fQ2     <- splinefun(m$x, m$y^2)
-  dfQ     <- splinefun(m$x, m$dy)
-  dfQ2    <- splinefun(m$x, m$dy^2)
-  factor  <- splinefun(m$x, m$y^2 + (1/mass^2) * m$dy^2)
-  list(fQ = fQ, fQ2 = fQ2, dfQ2 = dfQ2, factor = factor)
+  fm      <- splinefun(m$z, m$f)
+  fm2     <- splinefun(m$z, m$f^2)
+  dfm     <- splinefun(m$z, m$df)
+  dfm2    <- splinefun(m$z, m$df^2)
+  factor  <- splinefun(m$z, m$f^2 + (1/mass^2) * m$df^2)
+  list(fm = fm, fm2 = fm2, dfm2 = dfm2, factor = factor)
 }
 
 # these are the normalizable modes
-getU1NormalizableModeRaw <- function(zf = 5, mass = 3.5, alpha = 0) {
-  zUV <- exp(-A0)
-  # TODO: actually compute this
-  y0AdS  <- mass * zUV * besselJ(mass * zUV, 1)
-  dy0AdS <- mass * mass * zUV * besselJ(mass * zUV, 0)
+getU1NormalizableModeRaw <- function(mass = 1.0, alpha = 0) {
   flog.debug(paste('finding mode mass', mass, 'alpha', alpha))
   # first we need to compute ihqcd with bigger precision
   # set the constants in the IHQCD package
@@ -101,12 +104,31 @@ getU1NormalizableModeRaw <- function(zf = 5, mass = 3.5, alpha = 0) {
 
   # pass all the variables inside the environment of this function
   environment(odeFun) <- pre$env
+  # Set up parameters and boundary conditions
+  pars <- c(mass = mass, alpha = alpha)
+  y0AdS    <- mass * z[1] * besselJ(mass * z[1], 1)
+  dy0AdS   <- mass * mass * z[1] * besselJ(mass * z[1], 0)
+  yini <- c(y1 = y0AdS, y2 = dy0AdS)
   # get the solution as a data frame
-  m <- as.data.frame(bvpcol(yini = c(y = y0AdS, dy = dy0AdS), x = z, func = odeFun, yend = c(NA, 0), nmax = 100000, atol = 1e-9))
+  m <- as.data.frame(ode(y = yini, times = z, func = odeFun, parms = pars))
+  # Now we need to normalize the normalizable mode
+  # We do that by imposing df = 0 at some zf
+  f <- splinefun(m$time, m$y1)
+  # We now the value of zf that satisfied the BC condition
+  zf <- uniroot(f = function(x) f(x,deriv = 1),
+                lower = m$time[1], upper = m$time[length(m$time)])$root
+  z <- m$time[m$time < zf]
+  y1   <- m$y1[m$time < zf]
+  y2   <- m$y2[m$time < zf]
+  f <- splinefun(z,exp(pre$Asder1fun(z) - pre$Phider1fun(z))* y1 * y1)
+  C2 <- integrate(f = f, lower = z[1], upper = z[length(z)])$value
+  f <- y1 / sqrt(C2)
+  df <- y2 / sqrt(C2)
+  m <- data.frame(z, f, df)
   mode <- getFQNormalizableModes(mass, m)
   # roll back global changes
   postcompute(pre)
-  # This solution does not have the correct normalization
+  # This solution does not have the correct
   mode
 }
 
