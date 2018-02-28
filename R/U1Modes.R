c2ba85c02f05e7f40da31c18ddb22dedb1556ebc
Artur Amorim <arturj.c.amorimdesousa@gmail.com>
2018-02-28 14:51:03 +0000

    Cleaned the code for the computation of the modes.

--- R/U1Modes.R
+++ R/U1Modes.R
@@ -1,35 +1,29 @@
-  odeFun1 <- function(x, y, pars) {
-    dy2 <- Q2 * y[1] - (Asder1fun(x) - Phider1fun(x)) * y[2]
-    dy1 <- y[2]
-    list(c(dy1, dy2))
-  }
+odeFunAdS <- function(x, y, pars) {
+  dy1 <- y[2]
+  dy2 <- Q2 * y[1] + y[2] / x
+  list(c(dy1, dy2))
+}
 
-  odeFunAdS <- function(x, y, pars) {
-      dy1 <- y[2]
-      dy2 <- - mass * mass * y[1] + y[2] / x
-      list(c(dy1, dy2))
-  }
+odeFun1 <- function(x, y, pars) {
+  dy2 <- Q2 * y[1] - (Asder1fun(x) - Phider1fun(x)) * y[2]
+  dy1 <- y[2]
+  list(c(dy1, dy2))
+}
 
-  odeFun1NormalizableModes <- function(x, y, pars) {
-    dy1 <- y[2]
-    dy2 <- - mass * mass * y[1] - (Asder1fun(x) - Phider1fun(x)) * y[2]
-    list(c(dy1, dy2))
-  }
-
-  odeFun2 <- function(x, y, pars) {
-    # remark: we are using Q2=q^2 and not the usual -q^2 since we are using the GR metric convention for eta
-    dy2 <- Q2 * y[1] * fact0fun(x) - (Asder1fun(x) - Phider1fun(x)
+odeFun2 <- function(x, y, pars) {
+  # remark: we are using Q2=q^2 and not the usual -q^2 since we are using the GR metric convention for eta
+  dy2 <- Q2 * y[1] * fact0fun(x) - (Asder1fun(x) - Phider1fun(x)
             + 2 * alpha * (2 * Asder1fun(x) * Asder2fun(x) - Asder3fun(x)) / fact1fun(x)) * y[2]
-    dy1 <- y[2]
-    list(c(dy1, dy2))
-  }
+  dy1 <- y[2]
+  list(c(dy1, dy2))
+}
 
 postcompute <- function(pre) {
   # use the default solution globally again
   solve(pre$ihqcd, A0 = pre$originalPars$A0, h = pre$originalPars$h, zmax = pre$originalPars$zmax)
 }
 
-precompute <- function(alpha) {
+precompute <- function(Q2, alpha) {
   ihqcd <- iHQCD()
   solve(ihqcd, A0 = A0, h = 0.001, zmax = 20)
   Asfun        <- splinefun(z, As)
@@ -40,8 +34,7 @@ precompute <- function(alpha) {
   fact0fun     <- splinefun(z, (1 - 2 * exp(-2 * As) * alpha * Asder2) / (1 - 2 * exp(-2 * As) * alpha * Asder1^2))
   fact1fun     <- splinefun(z, exp(2 * As) - 2 * alpha * Asder2)
   originalPars <- list(A0 = A0, h = h, zmax = zmax)
-  list(ihqcd = ihqcd, originalPars = originalPars , env = environment(),
-       Asder1fun = Asder1fun, Phider1fun = Phider1fun)
+  list(ihqcd = ihqcd, originalPars = originalPars , env = environment())
 }
 
 # these are the non-normalizable modes
@@ -50,7 +43,7 @@ getU1NNModeRaw <- function(Q2 = 3.5, alpha = 0) {
   # first we need to compute ihqcd with bigger precision
   # set the constants in the IHQCD package
   # here a large zmax is used since we have been experiencing some boundary effect
-  pre <- precompute(Q2)
+  pre <- precompute(Q2, alpha)
   # this is useful for the non minimal coupling case
   # now we need to define the differential equation for the U(1) field modes
   odeFun <- odeFun1
@@ -79,12 +72,10 @@ getFQs <- function(Q2, m) {
 
 getFQNormalizableModes <- function(mass, m) {
   # also compute some useful combinations
-  fm      <- splinefun(m$z, m$f)
-  fm2     <- splinefun(m$z, m$f^2)
-  dfm     <- splinefun(m$z, m$df)
-  dfm2    <- splinefun(m$z, m$df^2)
-  factor  <- splinefun(m$z, m$f^2 + (1/mass^2) * m$df^2)
-  list(fm = fm, fm2 = fm2, dfm2 = dfm2, factor = factor)
+  fmsplin <- splinefun(m$z, m$f)
+  fm      <- Vectorize(function(x) if (x <= m$z[length(m$z)]) fmsplin(x) else 0)
+  dfm     <- Vectorize(function(x) if (x <= m$z[length(m$z)]) fmsplin(x, deriv = 1) else 0)
+  list(fm = fm, dfm = dfm)
 }
 
 # these are the normalizable modes
@@ -93,10 +84,10 @@ getU1NormalizableModeRaw <- function(mass = 1.0, alpha = 0) {
   # first we need to compute ihqcd with bigger precision
   # set the constants in the IHQCD package
   # here a large zmax is used since we have been experiencing some boundary effect
-  pre <- precompute(alpha)
+  pre <- precompute(-mass^2, alpha)
   # this is useful for the non minimal coupling case
   # now we need to define the differential equation for the U(1) field modes
-  odeFun <- odeFun1NormalizableModes
+  odeFun <- odeFun1
 
   # TODO - CHECK THAT IF ODEFUN2 NEEDS TO BE MODIFIED
   if (abs(alpha) > 0)
@@ -106,6 +97,8 @@ getU1NormalizableModeRaw <- function(mass = 1.0, alpha = 0) {
   environment(odeFun) <- pre$env
   # Set up parameters and boundary conditions
   pars <- c(mass = mass, alpha = alpha)
+  # In the UV warped AdS is the same as AdS
+  # So we use AdS solution as boundary condition
   y0AdS    <- mass * z[1] * besselJ(mass * z[1], 1)
   dy0AdS   <- mass * mass * z[1] * besselJ(mass * z[1], 0)
   yini <- c(y1 = y0AdS, y2 = dy0AdS)
@@ -117,10 +110,12 @@ getU1NormalizableModeRaw <- function(mass = 1.0, alpha = 0) {
   # We now the value of zf that satisfied the BC condition
   zf <- uniroot(f = function(x) f(x,deriv = 1),
                 lower = m$time[1], upper = m$time[length(m$time)])$root
-  z <- m$time[m$time < zf]
+  z    <- m$time[m$time < zf]
   y1   <- m$y1[m$time < zf]
   y2   <- m$y2[m$time < zf]
-  f <- splinefun(z,exp(pre$Asder1fun(z) - pre$Phider1fun(z))* y1 * y1)
+  ff <- function(z) exp(Asder1fun(z) - Phider1fun(z))
+  environment(ff) <- pre$env
+  f <- splinefun(z, ff(z) * y1 * y1)
   C2 <- integrate(f = f, lower = z[1], upper = z[length(z)])$value
   f <- y1 / sqrt(C2)
   df <- y2 / sqrt(C2)
