45f09d5c2adbc986b17bbfb66860d95a2bfb3124
Robert Carcassés Quevedo <carcasses@gmail.com>
2018-04-13 12:37:46 +0100

    Cleaning up

--- R/F2.R
+++ R/F2.R
@@ -109,7 +109,7 @@ fNNMC.F2 <- function(f2, Q2, x, J, wf, alpha) {
   # contribution from the longitudinal part
   t3fun   <- splinefun(z, (dfQ2(z) / Q2) * DparallelPsi(wf))
   integral <- integrate(function(x) t1fun(x) * (t2fun(x) + t3fun(x)), z[1], z[length(z)], stop.on.error = FALSE)
-  factor <- -1 / (tan(pi * J / 2) * 4 * pi^2 * 1 / 137)
+  factor <- 1 / (tan(pi * J / 2) * 4 * pi^2 * 1 / 137)
   factor * x^(1 - J) * Q2^J * integral$value
 }
 
